608dbe6040e45c4263d7eb054fe8ce2b26354abf
Gunski <gunnarkryth@gmail.com>
2024-12-20 12:01:59 +0100

    1

--- scripts/riveActions.js
+++ scripts/riveActions.js
@@ -1,13 +1,13 @@
 import { hp, isEating } from "./riveController.js";
 import { socket } from "./socket/socketController.js";
 
-let feedButton = document.getElementById("feedButton");
+// let feedButton = document.getElementById("feedButton");
 
 const feed = () => {
   // isEating.fire();
   socket.emit("feed", { feed: "Client has fed" });
 };
 
-feedButton.addEventListener("click", () => {
+riveCanvas.addEventListener("click", () => {
   feed();
 });
